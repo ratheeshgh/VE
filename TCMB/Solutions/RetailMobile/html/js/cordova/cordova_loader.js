b742fbec6cdae1eda0f7912a5214fa77c5329e11
Maxi Diaz <m.diaz@globallogic.com>
2017-08-08 18:18:42 -0300

    cambio menor

--- TCMB/Solutions/RetailMobile/html/js/cordova/cordova_loader.js
+++ TCMB/Solutions/RetailMobile/html/js/cordova/cordova_loader.js
@@ -6,7 +6,7 @@ if (!window.edgeCordovaLoaded) {
 	} 
 	
 	;(function () {
-		var isDroid = navigator.userAgent.match(/Android.*edgeConnectSmartHybrid/);
+		var isDroid = navigator.userAgent.match(/Android/);
 		var isiOS = navigator.userAgent.match(/(iPhone|iPod|iPad).*edgeConnectSmartHybrid/);
 		var isWP = isWindowsPhone();
 		var droidScripts = [
